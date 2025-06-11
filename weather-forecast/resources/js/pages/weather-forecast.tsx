61bebb04ded7d925d0cf56f5ef7f65682ac0e5e6
ste-h <27690822+ste-h@users.noreply.github.com>
2025-06-11 17:21:31 +1000

    Changed default route to weather-forecast

---
+++ weather-forecast/resources/js/pages/weather-forecast.tsx
@@ -0,0 +1,13 @@
+import { Head } from '@inertiajs/react';
+
+export default function Homepage() {
+    return (
+        <>
+            <Head title="Home" />
+            <div>
+                <h1>Temp</h1>
+                
+            </div>
+        </>
+    );
+}
